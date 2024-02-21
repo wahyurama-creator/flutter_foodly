bdbd7d71eb732be6cdfb6584f6364bf8a3ed8d35
wahyu rama <wahyuramadhon32@gmail.com>
2024-02-21 19:12:58 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_multi_vendor
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_multi_vendor/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_multi_vendor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
